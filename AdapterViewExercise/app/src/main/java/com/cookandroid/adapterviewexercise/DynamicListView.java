4555b7ebc479481eef56e3b3f73351f24fb7e9e9
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-11-14 14:57:15 +0900

    a

--- AdapterViewExercise/app/src/main/java/com/cookandroid/adapterviewexercise/DynamicListView.java
+++ AdapterViewExercise/app/src/main/java/com/cookandroid/adapterviewexercise/DynamicListView.java
@@ -1,6 +1,9 @@
 package com.cookandroid.adapterviewexercise;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DynamicListView extends Activity {
     @Override
@@ -18,50 +22,61 @@ public class DynamicListView extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_listview);
         setTitle("동적 리스트 뷰 테스트");
-        //자료구조에서 배운 ArrayList 를 사용하여 add, remove
-        //하여 리스트뷰를 동적으로 사용가능
-        //사용자가 수정 불가능한 딱 정해진 정적 리스트뷰를
-        //만들고 싶다면 ArrayList 대신 Stringp[ 배열 사용하면 됨.
+
+        // (자료구조에서 배운)ArrayList를 사용하여 add, remove
+        // 정적 리스트뷰(변경이 필요없는)를 구현하고 싶은 경우
+        // String[] 과 같이 배열을 사용하여 선언하면 됨.
         final ArrayList<String> list = new ArrayList<String>();
 
-        ListView lv = (ListView)findViewById(R.id.listView);
+        ListView listView = (ListView)findViewById(R.id.listView);
 
+        // 두 번째 파라미터(simple_list_item): 리스트뷰의 형식
+        // 세 번째 파라미터: 리스트뷰에 적용할 내용들(ArrayList)
         final ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(this,
+                new ArrayAdapter<String>(this,
                         android.R.layout.simple_list_item_1,
                         list);
+
         // listView에 어댑터를 사용하겠다고 연결
-        lv.setAdapter(adapter);
+        listView.setAdapter(adapter);
 
-        final EditText et = (EditText)findViewById(R.id.addItemText);
-        Button btn = (Button)findViewById(R.id.addListViewBtn);
+        final EditText edtItem = (EditText)findViewById(R.id.addItemText);
+        Button addBtn = (Button)findViewById(R.id.addListViewBtn);
 
-        // 리스트뷰를 동적으로 구성, ArrayList의 add()메서드 사용
-        //그리고 어댑터의 notifyDataSetchanged() 메소드로 갱신
-        btn.setOnClickListener(new View.OnClickListener() {
+        addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                list.add(et.getText().toString());
+                list.add(edtItem.getText().toString());
                 adapter.notifyDataSetChanged();
-            }
-        });
-        //inTiemClick의 입력 인자가 무려 4개
-        //어댑터 뷰, 뷰, i <- 리스트의 위치값, L <- id
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(getApplicationContext(), "리스트뷰의" + (position+1) + "번째 item클릭함",
-                Toast.LENGTH_LONG).show();
             }
         });
 
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // 입력인자(순서대로): 어댑터뷰, 뷰, 클릭항목의 순번, 항목의 아이디
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(DynamicListView.this,
+                        (i+1) + "번째 리스트뷰 선택됨",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), "리스트뷰의" + (i+1) + "번째 item클릭함",
-                Toast.LENGTH_LONG).show();
-                list.remove(i);
-                adapter.notifyDataSetChanged();
+                final int index = i;
+                AlertDialog.Builder dlg = new AlertDialog.Builder(DynamicListView.this);
+                dlg.setMessage((index+1) + "번째 리스트뷰: " + list.get(i).toString()
+                        +"\n 제거하시겠습니까?");
+                dlg.setNegativeButton("취소", null);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        list.remove(index);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                dlg.show();
                 return false;
             }
         });
--- AdapterViewExercise/app/src/main/java/com/cookandroid/adapterviewexercise/GridViewEx.java
+++ AdapterViewExercise/app/src/main/java/com/cookandroid/adapterviewexercise/GridViewEx.java
@@ -1,13 +1,16 @@
 package com.cookandroid.adapterviewexercise;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GridViewEx extends Activity {
     @Override
@@ -17,28 +20,29 @@ public class GridViewEx extends Activity {
         setTitle("그리드뷰 테스트");
 
         final GridView gv = (GridView)findViewById(R.id.gridView);
-        MyGridAdapter gridAdapter = new MyGridAdapter(this);
-        gv.setAdapter((gridAdapter));
+
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
     }
 
-    //1.onCreate{} 바깥쪽에 새로운 클래스를 생성
-    //BasAdapter 클래스를 상속(extends) 받는
-    public class MyGridAdapter extends BaseAdapter{
-        // 활용하고픈 생성자를 명시함
+    // 1. BaseAdapter의 상속을 받는 어댑터를 정의.
+    public class MyGridAdapter extends BaseAdapter {
+        // 2. 컨택스트 변수를 선언하고, this 컨택스트를 생성자에서 받아옴.
+        // 다른 메소드에서 context 변수를 사용하기 위함.
         Context ctx;
-        public MyGridAdapter(Context c){
+        public MyGridAdapter(Context c) {
             ctx = c;
         }
-        //2. Ctrl + i를 눌러서 ㅜ상메소드의 기본 형태를 명시
-        //3. 그 중에 필요한 getCount()와
-        //get View() 메소드만 재가공하여 사용
-        //4.그리드뷰를 포함시킬 이미지들을 정의
-        Integer posterid[] = {R.drawable.movie_image1, R.drawable.movie_image2, R.drawable.movie_image3, R.drawable.movie_image4, R.drawable.movie_image5
-        , R.drawable.movie_image6, R.drawable.movie_image7, R.drawable.movie_image8, R.drawable.movie_image9, R.drawable.movie_image10};
+
+        // 3. Ctrl + i 를 눌러서
+        // 추상메소드들의 기본 형태를 자동 완성
+
+        // 4. getCount() - 그리드뷰에 보일 이미지의 갯수 반환 - 와
+        // getView() - 영화 포스터를 각 그리드뷰 칸마다 이미지로 보여줌 - 메소드를
+        // 필요성에 맞게 수정
         @Override
         public int getCount() {
-            //posterid 변수의 길이만큼 (아마10이 반환)
-            return posterid.length;
+            return posterId.length;
         }
 
         @Override
@@ -51,17 +55,54 @@ public class GridViewEx extends Activity {
             return 0;
         }
 
-        //6. 영화 포스터를 각 그리드뷰 칸마다 이미지로 보여주는 메소드
-        // getView()가 getCount()에서의 넘겨받은 값만큼 반복
+        // 4-1. 영화 포스터들의 고유값을 배열 형태로 처리
+        Integer posterId[] = {R.drawable.movie_image1, R.drawable.movie_image2,
+                R.drawable.movie_image3, R.drawable.movie_image4,
+                R.drawable.movie_image5, R.drawable.movie_image6,
+                R.drawable.movie_image7, R.drawable.movie_image8,
+                R.drawable.movie_image9, R.drawable.movie_image10,
+                R.drawable.movie_image1, R.drawable.movie_image2,
+                R.drawable.movie_image3, R.drawable.movie_image4,
+                R.drawable.movie_image5, R.drawable.movie_image6,
+                R.drawable.movie_image7, R.drawable.movie_image8,
+                R.drawable.movie_image9, R.drawable.movie_image10,
+                R.drawable.movie_image1, R.drawable.movie_image2,
+                R.drawable.movie_image3, R.drawable.movie_image4,
+                R.drawable.movie_image5, R.drawable.movie_image6,
+                R.drawable.movie_image7, R.drawable.movie_image8,
+                R.drawable.movie_image9, R.drawable.movie_image10};
+
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            //get View()의 int i <- 포지션을 가리킴
+            // i = list의 포지션. 아래의 내용들이 getCount()만큼 반복된다고 생각하셈.
             ImageView iv = new ImageView(ctx);
-            iv.setLayoutParams(new GridView.LayoutParams(200, 300));
+            iv.setLayoutParams(new GridView.LayoutParams(200,300));
             iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
             iv.setPadding(5,5,5,5);
-            iv.setImageResource(posterid[i]);
-            return null;
+
+            iv.setImageResource(posterId[i]);
+
+            final int pos = i;
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(),
+                            "text:" + GridViewEx.this.toString() +
+                                    "/ and: " + getApplicationContext().toString(),
+                            Toast.LENGTH_LONG).show();
+                    View dialogView = (View)View.inflate
+                            (GridViewEx.this,
+                            R.layout.grid_dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(GridViewEx.this);
+                    ImageView bigPoster = (ImageView)dialogView.findViewById(R.id.bigPoster);
+                    bigPoster.setImageResource(posterId[pos]);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return iv;
         }
     }
 }
